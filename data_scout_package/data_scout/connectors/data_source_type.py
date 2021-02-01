549390fd69acebad97e24fdb11afca1a5a31bade
Jan Thiemen Postema <janthiemen.postema@milliman.com>
2021-02-01 19:38:27 +0100

    Move scouting to the package

---
+++ data_scout_package/data_scout/connectors/data_source_type.py
@@ -0,0 +1,23 @@
+from .connector import Connector
+from .csv import CSV
+from .big_query import BigQuery
+from ..exceptions import DataSourceConnectorUnavailableException
+
+
+class DataSourceType:
+    data_source_types = {"BigQuery": BigQuery, "CSV": CSV}
+
+    def serialize(self):
+        serialized = []
+        for data_source_type in self.data_source_types.values():
+            serialized.append({"name": data_source_type.__name__, "fields": data_source_type.fields})
+
+        return serialized
+
+    @staticmethod
+    def get_by_string(name: str) -> Connector:
+        data_source = DataSourceType.data_source_types.get(name)
+        if data_source is None:
+            raise DataSourceConnectorUnavailableException(name)
+        return data_source
+
