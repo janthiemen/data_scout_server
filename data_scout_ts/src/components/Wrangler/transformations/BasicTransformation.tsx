a0b2c43958f6830f3ac57ba82580bc6bb8acec45
Jan Thiemen Postema <janthiemen.postema@milliman.com>
2021-06-22 11:44:32 +0200

    Change folder names

--- data_scout_ts/src/components/Wrangler/transformations/BasicTransformation.tsx
+++
@@ -1,7 +0,0 @@
-import { IProps } from "@blueprintjs/core";
-
-export interface TranformationProps extends IProps {
-    newTransformation: (transformationType: string, kwargs: { [key: string]: any }) => void;
-}
-
-export interface TransformationState { }
---
+++ frontend/src/components/Wrangler/transformations/BasicTransformation.tsx
@@ -0,0 +1,7 @@
+import { IProps } from "@blueprintjs/core";
+
+export interface TranformationProps extends IProps {
+    newTransformation: (transformationType: string, kwargs: { [key: string]: any }) => void;
+}
+
+export interface TransformationState { }
